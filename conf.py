b8c4dfef54572447534b6d2adf656d3584e5d8ac
isaacgbaba <110928803+isaacgbaba@users.noreply.github.com>
2022-08-09 14:42:07 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Td Summer sucess program cs lab '
+copyright = '2022,  Isaac Gbaba'
+author = 'Isaac Gbaba'
 
 
 
@@ -49,8 +49,8 @@ with open ('info.yml','r') as f:
 
 
 html_theme_options = {
-  "github_url": "https://github.com/username/",
-  "twitter_url": "https://twitter.com/username",
+  "github_url": "https://github.com/isaacgbaba/",
+  "twitter_url": "https://twitter.com/izak2x",
   "search_bar_text": "Search this site...",
   "navbar_end": ["search-field.html"],
   "left_sidebar_end":[ "icon-links.html"]
